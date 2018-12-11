bb92b40dd441a5a572acd5fe06a342f8809e8652
xiayj <yuejie.xia@51shaoxi.com>
2018-12-11 18:46:44 +0800

    demo

--- src/test/java/java8/LambdaTest4.java
+++ src/test/java/java8/LambdaTest4.java
@@ -3,6 +3,7 @@ package java8;
 import org.junit.Test;
 
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 /**
  * @author: xiayuejie
@@ -13,10 +14,12 @@ public class LambdaTest4 {
 
     /**
      * 方法的引用
-     * 1.对象::方法名
-     * 2.类::方法名
-     * 3.对象::方法名
+     * 1.对象::实例方法名
+     * 2.类::静态方法名
+     * 3.类::实例方法名
      */
+
+    //一、
     @Test
     public void test1() {
         Consumer<Integer> consumer = x -> System.out.println(x);
@@ -24,6 +27,12 @@ public class LambdaTest4 {
         System.out.println("========================");
         Consumer<Integer> consumer1 = System.out::println;//对象名::方法名
         consumer1.accept(2000);
+
+        Employee employee = new Employee("张三", 19, 5000);
+        Supplier<String> supplier1 = () -> employee.getName();
+        System.out.println(supplier1.get());
+        Supplier<String> supplier2 = employee::getName;//对象名::方法名
+        System.out.println(supplier2.get());
     }
 
 }
